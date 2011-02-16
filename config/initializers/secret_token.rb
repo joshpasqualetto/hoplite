a145e92bb0e97e1b7fabcb1dcea3ddf49c6a8063
Josh Pasqualetto <joshua.pasqualetto@gmail.com>
2011-02-16 13:34:07 -0800

    Initial commit of hoplite

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hoplite::Application.config.secret_token = '2788d0ddf77046a6ebf312680cdabdbeb3a5671edf54e8b22d9bab79284ef923ec86821ddf71fba00e574c380d926e70dd6df5e9724713b3ebf521ea858a24cd'
