90be7f8d0c2a1316740de469255f52f9ec2f9a98
Josh Pasqualetto <joshua.pasqualetto@gmail.com>
2011-02-22 16:58:35 -0800

    Working Login & Backup Profile for Foundry switchs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+BackupProfile.create(:name => "Generic tftp backup", :description => "Generic tftp backup profile, Known working with: Foundry FastIron (03.4 firmware)", :script => "copy running-config tftp $SERVER_IP $DST_FILE\r\nexit\r\nexit")
+LoginProfile.create(:name => "Generic telnet login", :description => "Generic telnet login requiring en \"Super\" privileges, Known working with: Foundry FastIron switchs (03.4 firmware)", :script => "$PASSWORD\r\nen\r\n$PASSWORD")
